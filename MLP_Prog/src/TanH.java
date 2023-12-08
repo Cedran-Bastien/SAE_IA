9ec5c276676d953db9e70468f101b88bfcfe357c
cedran <cedran.bastien1@etu.univ-lorraine.fr>
2023-12-08 15:36:08 +0100

    Implement transpher functions

---
+++ MLP_Prog/src/TanH.java
@@ -0,0 +1,10 @@
+public class TanH implements TransferFunction{
+    @Override
+    public double evaluate(double value) {
+        return Math.tanh(value);    }
+
+    @Override
+    public double evaluateDer(double value) {
+        return 1 - Math.pow(evaluate(value),2);
+    }
+}
