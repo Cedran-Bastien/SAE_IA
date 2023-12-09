bca30db0f40f4d1ec12c3e7accff7d163b2700da
tian <1335857832@qq.com>
2023-12-09 10:27:13 +0100

    fonction ET

--- MLP_Prog/src/MainTest.java
+++ MLP_Prog/src/MainTest.java
@@ -6,18 +6,21 @@ public class MainTest {
                 new int[]{1,0,0},
                 new int[]{1,1,1}
         };
-        MLP mlp = new MLP(new int[]{2,16,8,4,2},0.001,new Sigmoide());
-        for (int i = 0; i<10;i++){
+        MLP mlp = new MLP(new int[]{2,16,8,4,1},0.001,new TanH());
+        double error = 1;
+        while (error > 0.1){
             for (int j =0;j<et.length;j++){
                 double[] input = new double[]{et[j][0],et[j][1]};
                 double[] output = new double[]{et[j][2]};
-                mlp.backPropagate(input,output);
+                error = mlp.backPropagate(input,output);
+                System.out.printf("loss: %f\n",error);
             }
         }
         for (int i = 0; i< et.length;i++){
             double[] input = new double[]{et[i][0],et[i][1]};
             double[] output = mlp.execute(input);
-            System.out.println("Fonction ET, input: "+input[0]+" "+input[1]+"\n, output: "+Math.max(output[0],output[1]);
+            boolean res = output[0] > 0.5;
+            System.out.println("Fonction ET, input: "+input[0]+" "+input[1]+", output: "+res);
         }
     }
 }
